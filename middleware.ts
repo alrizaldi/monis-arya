9df697511263acf27138a097d19158d8b9273266
alrizaldiSHS <10rizaldiakbar@gmail.com>
2026-05-29 21:30:43 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -13,7 +13,7 @@ export async function middleware(req: NextRequest) {
         getAll() {
           return req.cookies.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options: CookieOptions }[]) {
           cookiesToSet.forEach(({ name, value, options }) => {
             req.cookies.set(name, value);
             // Setting the cookie on the response
